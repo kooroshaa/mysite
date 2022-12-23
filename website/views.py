f49481a6f01fea3f8498d2d051da753c71cab923
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2022-12-23 17:05:49 +0330

    create TEMPLATES folder

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index_view(request):
-    return HttpResponse('<h1>HOME PAGE</h1>')
+    return render(request,'website/index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>ABOUT PAGE</h1>')
+    return render(request,'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>CONTACT PAGE</h1>')
+    return render(request,'website/contact.html')
