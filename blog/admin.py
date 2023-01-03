0a02bf847ead8f94efdddaeb4032c9c91463d03b
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-03 16:38:19 +0330

    register post

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from blog.models import Post
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    fields = ('title',)
+    list_display = ('title','counted_views','status','created_date','published_date')
+    list_filter = ('status',)
+    
+    search_fields = ['title', 'content']
+
+admin.site.register(Post,PostAdmin)
