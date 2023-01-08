5f90f2e9c4b921bbdcf4264fc2276f4a1750e66c
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-08 15:37:18 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from blog.models import Post
+from django.utils import timezone
 
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     context = {'posts' : posts}
     return render(request,'blog/blog-home.html',context)
 
 def single_view(request):
     return render(request,'blog/blog-single.html')
+
