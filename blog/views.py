25de1f8392b5a1dee762c6b0495c39c6d95cb7cd
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-09 13:30:19 +0330

    nothing

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,15 @@ from django.utils import timezone
 def blog_view(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
     context = {'posts': posts}
+    '''
     def blog_post(request, post_id):
         blog_object = Post.objects.get(id=post_id)
         blog_object.counted_views = blog_object.counted_views+1
         blog_object.save()
+    '''
 
     return render(request, 'blog/blog-home.html', context)
 
 
-def single_view(request):
+def single_view(request,pid):
     return render(request, 'blog/blog-single.html')
