bc6be881d12be38943a3d51482b84b8638266d7c
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-09 11:40:01 +0330

    tamrin
    +add counted view increment function
    +add filter to published_date

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
 
+
 def blog_view(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
-    context = {'posts' : posts}
-    return render(request,'blog/blog-home.html',context)
+    context = {'posts': posts}
+    def blog_post(request, post_id):
+        blog_object = Post.objects.get(id=post_id)
+        blog_object.counted_views = blog_object.counted_views+1
+        blog_object.save()
+
+    return render(request, 'blog/blog-home.html', context)
+
 
 def single_view(request):
-    return render(request,'blog/blog-single.html')
-
+    return render(request, 'blog/blog-single.html')
