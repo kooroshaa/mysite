b0752c4a9aa5e942a95d5b9057ae37dee1c7c5d3
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-09 14:24:18 +0330

    Revert "tamrin"
    This reverts commit bc6be881d12be38943a3d51482b84b8638266d7c.

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
 
-
 def blog_view(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
+<<<<<<< HEAD
+    context = {'posts' : posts}
+    return render(request,'blog/blog-home.html',context)
+
+def single_view(request):
+    return render(request,'blog/blog-single.html')
+
+=======
     context = {'posts': posts}
     '''
     def blog_post(request, post_id):
@@ -18,3 +25,4 @@ def blog_view(request):
 
 def single_view(request,pid):
     return render(request, 'blog/blog-single.html')
+>>>>>>> parent of 73fb2e3 (Revert "nothing")
