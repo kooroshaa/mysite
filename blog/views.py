2cf7dae709e495bca711ea6e97ac845b8a8fd71d
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-21 14:50:53 +0330

    end of 6 chapter6

--- blog/views.py
+++ blog/views.py
@@ -13,19 +13,3 @@ def single_view(request,pid):
     context = {'post' : post}
     return render(request,'blog/blog-single.html',context)
 
-def next_previous(request):
-    posts = Post.objects.all()  # fetching all post objects from database
-    p = Paginator(posts, 5)  # creating a paginator object
-    # getting the desired page number from url
-    page_number = request.GET.get('page')
-    try:
-        page_obj = p.get_page(page_number)  # returns the desired page object
-    except PageNotAnInteger:
-        # if page_number is not an integer then assign the first page
-        page_obj = p.page(1)
-    except EmptyPage:
-        # if page is empty then return last page
-        page_obj = p.page(p.num_pages)
-    context = {'page_obj': page_obj}
-    # sending the page object to index.html
-    return render(request, 'index.html', context)
