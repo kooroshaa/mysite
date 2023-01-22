cb4fbc1425785e67c6d47c756fc9edf4be345d49
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-22 16:34:40 +0330

    post categories

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post,Category
 
 register = template.Library()
 
@@ -7,3 +7,14 @@ register = template.Library()
 def latastposts():
     posts = Post.objects.filter(status=1).order_by('published_date')[:2]
     return {'posts' : posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category = name).count()
+    return {'categories' : cat_dict}
+
+
