5931ea7ebb7be0451e6ad5d8d9b3a65742e12a81
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-01-24 14:48:19 +0330

    one view with many urls

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
-from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-def blog_view(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now())
-    context = {'posts' : posts}
-    return render(request,'blog/blog-home.html',context)
 
-def single_view(request,pid):
-    post = get_object_or_404(Post,pk=pid,status=1)
-    context = {'post' : post}
-    return render(request,'blog/blog-single.html',context)
-
-def blog_category(request,cat_name):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
-    posts = posts.filter(category__name = cat_name)
-    context = {'posts' : posts}
-    return render(request,'blog/blog-home.html',context)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
+
+def single_view(request, pid):
+    post = get_object_or_404(Post, pk=pid, status=1)
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
+
+
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
 
 def test(request):
-    return render(request,'test.html')
-
+    return render(request, 'test.html')
