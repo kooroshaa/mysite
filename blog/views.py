8eba818f0490b44a58224729657601b46fa66c95
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-02-12 22:04:10 +0330

    search

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,14 @@ def blog_category(request, cat_name):
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'):
+            posts=posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
+
 
 def test(request):
     return render(request, 'test.html')
