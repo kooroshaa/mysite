60d647ae7daf8bf306336752d6e8836f45f9d647
Javad Amirahmadi <amirahmadi.jk@gmail.com>
2023-02-18 14:55:05 +0330

    paginate in view

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,16 @@ def blog_view(request, **kwargs):
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
+
+    posts = Paginator(posts,1)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
